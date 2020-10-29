8ada42ac06e873e3cd2efc8e10a32ae181b9e6d0
marcos pinardi <exegeses@gmail.com>
2020-10-28 21:15:13 -0300

    getters & setters

--- clase1/Persona.php
+++ clase1/Persona.php
@@ -3,9 +3,40 @@
     class Persona
     {
         ## atributos
-        public $nombre;
-        public $apellido;
+        private $nombre;
+        private $apellido;
 
         ## métodos
-        
+
+        /**
+         * @return mixed
+         */
+        public function getNombre()
+        {
+            return $this->nombre;
+        }
+
+        /**
+         * @param mixed $nombre
+         */
+        public function setNombre($nombre): void
+        {
+            $this->nombre = $nombre;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getApellido()
+        {
+            return $this->apellido;
+        }
+
+        /**
+         * @param mixed $apellido
+         */
+        public function setApellido($apellido): void
+        {
+            $this->apellido = $apellido;
+        }
     }
---
+++ clase1/vistaPersona.php
@@ -0,0 +1,21 @@
+<?php
+    require '../config/funciones.php';
+    require 'Persona.php';
+    //instanciar
+    $Persona = new Persona;
+
+    mostrar($Persona);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+<h1>Vista persona</h1>
+
+</body>
+</html>
