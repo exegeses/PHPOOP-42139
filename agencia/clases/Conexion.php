46f63e5902b1f87980ce18e522fb72d3647a1c07
marcos pinardi <exegeses@gmail.com>
2020-11-04 21:22:40 -0300

    método estático conectar() + singleton

--- agencia/clases/Conexion.php
+++ agencia/clases/Conexion.php
@@ -3,4 +3,20 @@
     class Conexion
     {
 
+        private static $link;
+
+        private function __construct()
+        {}
+
+        static function conectar()
+        {
+            if ( !isset( self::$link ) ){
+                self::$link = new PDO(
+                    'mysql:host=localhost;dbname=agencia',
+                    'root',
+                    'root'
+                );
+            }
+            return self::$link;
+        }
     }
