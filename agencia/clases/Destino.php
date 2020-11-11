cb4491dc9a6cb636a50bcfc2440475aba937849a
marcos pinardi <exegeses@gmail.com>
2020-11-11 20:40:23 -0300

    panel de administración de destinos

--- agencia/clases/Destino.php
+++ agencia/clases/Destino.php
@@ -2,5 +2,141 @@
 
     class Destino
     {
+        private $destID;
+        private $destNombre;
+        private $regID;
+        private $destPrecio;
+        private $destAsientos;
+        private $destDisponibles;
+        private $destActivo;
 
+        public function listarDestinos()
+        {
+            $link = Conexion::conectar();
+            $sql = "SELECT destID, destNombre, 
+                            d.regID, r.regNombre, 
+                            destPrecio, 
+                            destAsientos, destDisponibles, 
+                            destActivo 
+                        FROM destinos d, regiones r
+                        WHERE d.regID = r.regID";
+            $stmt = $link->prepare($sql);
+            $stmt->execute();
+            $destinos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $destinos;
+        }
+        
+        ###########################
+        #### getters & setters
+        /**
+         * @return mixed
+         */
+        public function getDestID()
+        {
+            return $this->destID;
+        }
+
+        /**
+         * @param mixed $destID
+         */
+        public function setDestID($destID): void
+        {
+            $this->destID = $destID;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestNombre()
+        {
+            return $this->destNombre;
+        }
+
+        /**
+         * @param mixed $destNombre
+         */
+        public function setDestNombre($destNombre): void
+        {
+            $this->destNombre = $destNombre;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getRegID()
+        {
+            return $this->regID;
+        }
+
+        /**
+         * @param mixed $regID
+         */
+        public function setRegID($regID): void
+        {
+            $this->regID = $regID;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestPrecio()
+        {
+            return $this->destPrecio;
+        }
+
+        /**
+         * @param mixed $destPrecio
+         */
+        public function setDestPrecio($destPrecio): void
+        {
+            $this->destPrecio = $destPrecio;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestAsientos()
+        {
+            return $this->destAsientos;
+        }
+
+        /**
+         * @param mixed $destAsientos
+         */
+        public function setDestAsientos($destAsientos): void
+        {
+            $this->destAsientos = $destAsientos;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestDisponibles()
+        {
+            return $this->destDisponibles;
+        }
+
+        /**
+         * @param mixed $destDisponibles
+         */
+        public function setDestDisponibles($destDisponibles): void
+        {
+            $this->destDisponibles = $destDisponibles;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestActivo()
+        {
+            return $this->destActivo;
+        }
+
+        /**
+         * @param mixed $destActivo
+         */
+        public function setDestActivo($destActivo): void
+        {
+            $this->destActivo = $destActivo;
+        }
     }
