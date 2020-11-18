60de6317fde7ac7f3024a996eb75ae8e557ac4f7
marcos pinardi <exegeses@gmail.com>
2020-11-18 20:17:01 -0300

    vista de alta de una región

---
+++ agencia/agregarRegion.php
@@ -0,0 +1,19 @@
+<?php
+    require 'config/config.php';
+    include 'includes/over-all-header.html';
+    include 'includes/nav.php';
+?>
+
+    <main class="container">
+
+        <h1>Alta de una región</h1>
+
+            <div class="alert alert-success border-success shadow-sm col-6 mx-auto p-4">
+                Región .... agregadad correctamente.
+            </div>
+
+    </main>
+
+<?php
+    include 'includes/footer.php';
+?>
